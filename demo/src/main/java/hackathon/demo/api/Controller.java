98157122c165dacee76234d53ebf68c2fe1b0d84
baswunderlich <bastian.wunderlich@protonmail.com>
2023-01-13 23:11:40 +0100

    add build

--- demo/src/main/java/hackathon/demo/api/Controller.java
+++ demo/src/main/java/hackathon/demo/api/Controller.java
@@ -2,11 +2,13 @@ package hackathon.demo.api;
 
 import hackathon.demo.calculator.Calculator;
 import hackathon.demo.model.Answer;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.FileNotFoundException;
 
+@CrossOrigin(origins = "*")
 @RestController
 public class Controller {
     @GetMapping("answer")
